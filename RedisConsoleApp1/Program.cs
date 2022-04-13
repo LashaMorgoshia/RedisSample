c2ea05322494375ccc7c1369b29b59f26fa93443
LashaMorgoshia <lashamor@gmail.com>
2022-04-14 00:31:04 +0400

    Add project files.

---
+++ RedisConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ServiceStack.Redis;
+
+public class Program
+{
+    public static void Main()
+    {
+        SaveData("localhost", "test", "demoValue");
+
+        Console.WriteLine(GetData("localhost", "test"));
+
+        Console.ReadLine();
+    }
+
+
+    private static void SaveData(string host, string key, string value)
+    {
+        using (RedisClient client = new RedisClient(host))
+        {
+            if (client.Get(key) == null)
+                client.Set(key, value);
+        }
+    }
+
+    private static string GetData(string host, string key)
+    {
+        using (RedisClient client = new RedisClient(host))
+        {
+            return client.Get<string>(key);
+        }
+    }
+}
